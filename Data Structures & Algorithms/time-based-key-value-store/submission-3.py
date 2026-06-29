4f1bde3c0bc31866ac8ee33e547858ca73380b5a
Mohammad Sabur Ali Mahdi <143124747+MohammadMahdi10@users.noreply.github.com>
2026-06-29 17:12:37 +0100

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,24 @@
+class TimeMap:
+    def __init__(self):
+        self.timeMap = {} # timestamp : {key : value}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.timeMap:
+            self.timeMap[key].append([value, timestamp])
+        else:
+            self.timeMap[key] = [[value, timestamp]]
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.timeMap:
+            l, r = 0, len(self.timeMap[key]) - 1
+
+            while l <= r:
+                mid = (l + r) // 2
+                if self.timeMap[key][mid][1] == timestamp:
+                    return self.timeMap[key][mid][0]
+                elif self.timeMap[key][mid][1] == timestamp:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+                
+        return self.timeMap[key][mid - 1][0]
