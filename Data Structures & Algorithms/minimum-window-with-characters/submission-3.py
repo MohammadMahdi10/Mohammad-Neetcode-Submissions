2332007f92ba9fbce7bc87c4605bdf1ac48411a3
Mohammad Sabur Ali Mahdi <143124747+MohammadMahdi10@users.noreply.github.com>
2026-07-05 15:31:02 +0100

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        sMap = {}
+        tMap = {}
+
+        for i in range(len(t)):
+            tMap[t[i]] = tMap.get(t[i], 0) + 1
+        
+        have, need = 0, len(tMap)
+
+        l = 0
+        res = float("infinity")
+        resList = [-1, -1]
+
+        for r in range(len(s)):
+            sMap[s[r]] = sMap.get(s[r], 0) + 1
+            if s[r] in tMap and sMap[s[r]] == tMap[s[r]]:
+                have += 1
+
+            while have == need:
+                if res > (r - l + 1):
+                    res = r - l + 1
+                    resList = [l, r]
+                
+                sMap[s[l]] -= 1
+                
+                if s[l] in tMap and sMap[s[l]] < tMap[s[l]]:
+                    have -= 1
+                l += 1
+
+        if res == float("infinity"):
+            return ""
+        else:
+            l, r = resList
+            return s[l:r+1]
