ca5051a2722737695d4994f3435a4ebef56574d7
Mohammad Sabur Ali Mahdi <143124747+MohammadMahdi10@users.noreply.github.com>
2026-07-08 20:40:58 +0100

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        head = dummy
+        curr = head
+
+        curr1 = l1
+        curr2 = l2
+
+        div = 0
+        while curr1 and curr2:
+            rem = 0
+
+            sums = curr1.val + curr2.val
+            if (sums) >= 10:
+                rem = sums - 10
+                curr.next = ListNode(rem + div)
+                curr = curr.next
+                div = sums // 10
+            else:
+                curr.next = ListNode(sums + div)
+                curr = curr.next
+                div = sums // 10
+        
+            curr1 = curr1.next
+            curr2 = curr2.next
+        
+            a = None
+            if curr1:
+                a = curr1
+            else:
+                a = curr2
+
+        while a:
+            rem = 0
+            sums = a.val + div
+            if (sums) >= 10:
+                div = 0
+                rem = sums - 10
+                curr.next = ListNode(rem + div)
+                curr = curr.next
+                div = sums // 10
+            else:
+                curr.next = ListNode(sums)
+                curr = curr.next
+        
+            a = a.next
+        
+        if div != 0:
+            curr.next = ListNode(div)
+
+        return head.next
