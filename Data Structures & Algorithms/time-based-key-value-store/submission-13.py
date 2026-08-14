17ea7eefce201ce1f5e9273752b8a25ee27395a8
Mohammad Sabur Ali Mahdi <143124747+MohammadMahdi10@users.noreply.github.com>
2026-08-14 15:11:01 +0100

    Add: time-based-key-value-store - submission-13

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-13.py
@@ -0,0 +1,30 @@
+class TimeMap:
+    def __init__(self):
+        self.contains = {}
+
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.contains:
+            self.contains[key] = []
+        self.contains[key].append((value, timestamp))
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        if key in self.contains:
+            arr = self.contains[key]
+            target = timestamp
+            l, r = 0, len(arr) - 1
+
+            while l <= r:
+                mid = (l + r) // 2
+                if arr[mid][1] == target:
+                    res = arr[mid][0]
+                    break
+                elif arr[mid][1] > target:
+                    r = mid - 1
+                else:
+                    res = arr[mid][0]
+                    l = mid + 1    
+    
+        return res
